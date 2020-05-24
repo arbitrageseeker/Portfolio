e7d56893adf1c3ab631d15dfc4d57d53e7f84436
Anssi Salomaa <anssi.20@hotmail.com>
2020-05-24 20:44:41 +0300

    fixed make

--- r/make.R
+++ r/make.R
@@ -5,6 +5,7 @@ in_dir <- readLines("in_dir.txt")
 
 source("r/selenium_scrape.R", encoding = "UTF-8")
 source("r/transactions.R", encoding = "UTF-8")
+source("r/fundamentals.R", encoding = "UTF-8")
 source("r/dividends_and_splits.R", encoding = "UTF-8")
 source("r/daily_prices.R", encoding = "UTF-8")
 source("r/data_proc.R", encoding = "UTF-8")
